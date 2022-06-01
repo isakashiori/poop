0c86fe3f6e0fe9807d3569ecb89f9e98bc899c1d
isakashiori <isslamdunk0000@gmail.com>
2022-06-01 20:40:47 +0900

    five times commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user
+    
+    private
+    
+    def cuurent_user
+        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+    
 end
